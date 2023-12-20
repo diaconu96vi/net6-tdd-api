14671df9c85bbc34198bc4b5af690630e7b432bc
diaconu96vi <diaconu.ionut96@gmail.com>
2023-12-20 15:01:24 +0100

    Create UsersController. Write first unit test using FluentAssertions.

---
+++ src/CloudCustomers.API/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudCustomers.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+
+    public UsersController()
+    {
+    }
+
+    [HttpGet(Name = "GetUsers")]
+    public async Task<IActionResult> Get()
+    {
+        return Ok("all good");
+    }
+}
---
+++ tests/CloudCustomers.UnitTests/Systems/Controllers/TestUsersController.cs
@@ -0,0 +1,22 @@
+using CloudCustomers.API.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudCustomers.UnitTests.Systems.Controllers;
+
+public class TestUsersController
+{
+    [Fact]
+    public async Task Get_OnSuccess_ReturnsStatusCode200()
+    {
+        // Arrange
+        var sut = new UsersController();
+
+        // Act
+        var result = (OkObjectResult)await sut.Get();
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+
+    }
+}
